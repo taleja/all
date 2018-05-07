2b28cfa5770245aacab21b2b6484ec21acc8a972
olena <olena>
2018-05-07 21:58:31 +0300

    refactoring

--- codeWars/src/main/java/codeWars/MorseCodeDecoder.java
+++ codeWars/src/main/java/codeWars/MorseCodeDecoder.java
@@ -9,37 +9,47 @@ public class MorseCodeDecoder {
 	}
 
 	public static String decode(String morseCode) {
-		StringBuilder sb = new StringBuilder();
+		StringBuilder buffer = new StringBuilder();
 		StringBuilder result = new StringBuilder();
 
 		for (int i = 0; i < morseCode.length(); i++) {
 
-			if (!Character.isWhitespace(morseCode.charAt(i))) {
-				sb.append(morseCode.charAt(i));
+			if (isThreeWhitespaces(morseCode, i)) {
+				consumeWord(buffer, result);
+			} else if (Character.isWhitespace(morseCode.charAt(i))) {
+				consumeCharacter(buffer, result);
 			} else {
-				if (i + 2 < morseCode.length() && Character.isWhitespace(morseCode.charAt(i + 1)) &&
-						Character.isWhitespace(morseCode.charAt(i + 2))) {
-					if (sb.length() > 0) {
-						result.append(Decoder.convertFromMorseToEnglish(sb.toString())).append(" ");
-						sb.setLength(0);
-					}
-				} else {
-					if (sb.length() > 0) {
-						result.append(Decoder.convertFromMorseToEnglish(sb.toString()));
-						sb.setLength(0);
-					}
-				}
+				consumeSignal(morseCode, buffer, i);
 			}
-			if (i == morseCode.length() - 1) {
-				if (sb.length() > 0) {
-					result.append(Decoder.convertFromMorseToEnglish(sb.toString()));
-					sb.setLength(0);
-				}
-			}
+
 		}
+		consumeCharacter(buffer, result);
+
 		System.out.println(result.toString());
 		return result.toString();
 	}
 
+	private static void consumeSignal(String morseCode, StringBuilder buffer, int i) {
+		buffer.append(morseCode.charAt(i));
+	}
+
+	private static boolean isThreeWhitespaces(String morseCode, int i) {
+		return i + 2 < morseCode.length() && Character.isWhitespace(morseCode.charAt(i))
+				&& Character.isWhitespace(morseCode.charAt(i + 1)) && Character.isWhitespace(morseCode.charAt(i + 2));
+	}
+
+	private static void consumeWord(StringBuilder buffer, StringBuilder result) {
+		if (buffer.length() > 0) {
+			result.append(Decoder.convertFromMorseToEnglish(buffer.toString())).append(" ");
+			buffer.setLength(0);
+		}
+	}
+
+	private static void consumeCharacter(StringBuilder buffer, StringBuilder result) {
+		if (buffer.length() > 0) {
+			result.append(Decoder.convertFromMorseToEnglish(buffer.toString()));
+			buffer.setLength(0);
+		}
+	}
 
 }
